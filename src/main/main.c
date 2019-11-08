1c36ba3f233b70e1d151fc93e01c74f3831c77e1
Elias Kauppi <11910622+kauppie@users.noreply.github.com>
2019-11-08 16:44:53 +0200

    Now saves time progress
    Time is now written periodically to onboard EEPROM, so removing the source of power doesn't reset clock.

--- src/main/main.c
+++ src/main/main.c
@@ -11,6 +11,8 @@
 #include <util/delay.h>
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <time.h>
 
 #include <math.h>
 
@@ -29,12 +31,18 @@
 #define DIGIT_COLUMNS 3
 #define PI 3.14159265358979323846
 
+// Time constants.
+#define TIME_ADDRESS 0x100
+#define TIME_WRITE_PERIOD_MS 30000
+
 // Macro functions.
 #define MIN(X, Y) (X < Y ? X : Y)
 #define MAX(X, Y) (X < Y ? Y : X)
 
 // Flags.
 #define BUTTON_STATE 0x01
+#define SLEEP_STATE 0x02
+#define USE_DIGITAL_DISPLAY_STATE 0x04
 
 // Global interrupt controlled volatiles.
 volatile uint_fast16_t usecs;
@@ -42,17 +50,22 @@ volatile uint_fast32_t msecs;
 volatile uint8_t stateFlags;
 volatile uint8_t prevStateFlags;
 volatile uint8_t buttonPressTime;
+volatile uint_fast32_t lastTimeWrite;
 
-// Digits used in digital clock scaled to 3x5 matrix as bits.
-const uint16_t digits[10] = {0x7B6F, 0x2492, 0x73E7, 0x72CF, 0x5BC9, 0x79CF, 0x79EF, 0x7249, 0x7BEF, 0x7BCF};
+// Digits used in digital clock scaled to 5x3 matrix as bits.
+const uint16_t digits[10] = {0x7B6F, 0x2492, 0x73E7, 0x73CF, 0x5BC9, 0x79CF, 0x79EF, 0x7249, 0x7BEF, 0x7BCF};
 // Rendering target and display source.
 uint16_t screen[ROWS];
 
 // Function declarations.
 void setup();
-void tickSleep();
-void debounce();
-void checkButton();
+void powerDown();
+void writeTimeToEEPROM();
+void readTimeFromEEPROM();
+static inline void debounce();
+inline void setState(uint8_t);
+inline void clearState(uint8_t);
+inline void toggleState(uint8_t);
 void updateButtonState();
 void clearAll();
 void clearForDraw();
@@ -67,18 +80,20 @@ void clearScreen();
 int main()
 {
 	setup();
-	uint8_t digital = 0;
 	
 	while (1) {
+		
+		if (stateFlags & SLEEP_STATE)
+			powerDown();
+		
 		clearScreen();
 		
-		if (buttonPressTime & 0x07) { digital = !digital; buttonPressTime = 0; }
-		
-		if (digital)
+		if (stateFlags & USE_DIGITAL_DISPLAY_STATE)
 			renderDigitalClockOnScreen();
 		else
 			renderAnalogClockOnScreen();
-			
+		
+		// Display screen with preset brightness.
 		for (uint8_t i = 0; i < ROWS; ++i) {
 			clearForDraw();
 			_delay_us(ROW_DELAY_US / 100 * (100 - LED_DUTY_CYCLE));
@@ -88,6 +103,37 @@ int main()
 		}
 		// Prevent the last row blasting at full brightness.
 		clearForDraw();
+	}
+}
+
+// Timed interrupt which increments the clock and checks for
+// button press.
+ISR(TIMER1_OVF_vect) {
+	usecs += 531;
+	msecs += 529 + usecs / 1000;
+	usecs %= 1000;
+	// Reset after 24 hours.
+	msecs %= 86400000;
+	
+	if (msecs > lastTimeWrite + TIME_WRITE_PERIOD_MS) {
+		lastTimeWrite = msecs;
+		writeTimeToEEPROM();
+	}
+	
+	updateButtonState();	
+	if (stateFlags & BUTTON_STATE) {
+		++buttonPressTime;
+	}
+	else buttonPressTime = 0;
+	
+	if (buttonPressTime == 1) {
+		toggleState(USE_DIGITAL_DISPLAY_STATE);
+	}
+	else if (buttonPressTime == 6) {
+		setState(SLEEP_STATE);
+	}
+	else if (stateFlags & SLEEP_STATE && (buttonPressTime == 3)) {
+		clearState(SLEEP_STATE);
 	}
 }
 
@@ -106,11 +152,19 @@ void setup() {
 	
 	// Reset globals and update button state.
 	usecs = 0;
-	msecs = 54660000;
-	stateFlags = 0;
-	prevStateFlags = 0;
+	readTimeFromEEPROM();
+	lastTimeWrite = msecs;
+	
+	stateFlags = USE_DIGITAL_DISPLAY_STATE;
+	prevStateFlags = stateFlags;
+	
 	buttonPressTime = 0;
 	updateButtonState();
+	
+	// Sleep enable with default idle mode.
+	MCUCR |= (1 << SE);
+	// Disable analog comparator.
+	ACSR |= (1 << ACD);
 	
 	// Set 1/64 prescaler for Timer 1 for 125kHz frequency at 8Mhz.
 	TCCR1B |= (1 << CS10) | (1 << CS11);
@@ -120,64 +174,62 @@ void setup() {
 	sei();
 }
 
-// Timed interrupt which increments the clock and checks for
-// button press.
-ISR(TIMER1_OVF_vect) {
-	usecs += 531;
-	msecs += 529 + usecs / 1000;
-	usecs %= 1000;
-	// Reset after 24 hours.
-	msecs %= 86400000;
-	
-	updateButtonState();
-	if (stateFlags & BUTTON_STATE) {
-		buttonPressTime <<= 1;
-		++buttonPressTime;
+void powerDown() {
+	cli();
+	while (stateFlags & SLEEP_STATE) {
+		sleep_enable();
+		sei();
+		sleep_cpu();
+		sleep_disable();
 	}
-	else 
-		buttonPressTime = 0;
+	sei();
 }
 
-void tickSleep() {
-	sleep_mode();
+void writeTimeToEEPROM() {
+	// Don't clear interrupt flag so that time stays relatively accurate.
+	// Write process can fail, but it is updated often enough it doesn't
+	// matter.
+	//cli();
+	eeprom_busy_wait();
+	eeprom_write_dword((uint32_t*)TIME_ADDRESS, msecs);
+	//sei();
 }
 
-void debounce() {
+void readTimeFromEEPROM() {
+	eeprom_busy_wait();
+	msecs = eeprom_read_dword((uint32_t*)TIME_ADDRESS);
+}
+
+static inline void debounce() {
 	_delay_ms(DEBOUNCE_TIME_MS);
 }
 
+inline void setState(uint8_t flags) {
+	prevStateFlags = stateFlags;
+	stateFlags |= flags;
+}
+
+inline void clearState(uint8_t flags) {
+	prevStateFlags = stateFlags;
+	stateFlags &= ~flags;
+}
+
+inline void toggleState(uint8_t flags) {
+	prevStateFlags = stateFlags;
+	stateFlags ^= flags;
+}
+
 void updateButtonState() {
-	// Make sure input pull-up is disabled. Makes visual glitches disappear
+	// Make sure input pull-up is enabled. Makes visual glitches disappear
 	// and button isn't self triggered.
 	PORTD |= (1 << PD2);
 	// Set button pin as input.
 	DDRD &= ~(1 << PD2);
-	// Set previous state.
-	prevStateFlags = stateFlags;
 	// If button is pressed.
 	if (~PIND & (1 << PD2))
-		stateFlags |= BUTTON_STATE;
+		setState(BUTTON_STATE);
 	else
-		stateFlags &= ~BUTTON_STATE;
-	// Set button pin back to output.
-	DDRD |= (1 << PD2);
-}
-
-void checkButton() {
-	// Make sure input pull-up is disabled. Doesn't really matter in the terms
-	// of working properly, but is disabled because it isn't needed.
-	PORTD |= (1 << PD2);
-	// Set button pin as input.
-	DDRD &= ~(1 << PD2);
-	
-	// If button is pressed.
-	if (~PIND & (1 << PD2)) {
-		clearForDraw();
-		debounce();
-		// Wait to not press.
-		while(~PIND & (1 << PD2));
-		debounce();
-	}
+		clearState(BUTTON_STATE);
 	// Set button pin back to output.
 	DDRD |= (1 << PD2);
 }
@@ -189,7 +241,8 @@ void clearScreen() {
 }
 
 void renderDigitalClockOnScreen() {
-	uint16_t seconds = msecs / 1000;
+	// Must be 32-bit, otherwise overflows at 18:18.
+	uint32_t seconds = msecs / 1000;
 	uint16_t minutes = seconds / 60;
 	seconds %= 60;
 	uint16_t hours = minutes / 60;
@@ -288,7 +341,8 @@ void drawLine(float x1, float y1, float x2, float y2)
 
 void renderAnalogClockOnScreen()
 {
-	uint16_t seconds = msecs / 1000;
+	// Must be 32-bit, otherwise overflows at 18:18.
+	uint32_t seconds = msecs / 1000;
 	uint16_t minutes = seconds / 60;
 	seconds %= 60;
 	uint16_t hours = minutes / 60;
